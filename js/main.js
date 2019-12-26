2aaded7704472bc8346efd610e4fa69d294b3e07
vivian <vivian820225@gmail.com>
2019-12-26 15:42:16 +0800

    12/25-feedback modify

--- js/main.js
+++ js/main.js
@@ -26,6 +26,16 @@ $(document).ready(function() {
       $(".topbar").removeClass("fixed");
     }
   });
+
+  // 瀑布流
+  var grid = $('.grid').imagesLoaded( function() {  
+    // init Masonry after all images have loaded  
+    grid.masonry({
+      columnWidth: '.grid-item',
+      itemSelector: '.grid-item',
+      percentPosition: true
+    });
+});
 });
 
 $(".main-menu a").on("click", function() {
@@ -43,6 +53,11 @@ function eventTime() {
   TweenMax.to("#event-time", 0.5, { delay: 0.1, alpha: 1 });
 }
 
+function howToPlay() {
+  TweenMax.to("#howtoplay", 0, { top: 0, ease: Quart.easeOut });
+  TweenMax.to("#howtoplay", 0.5, { delay: 0.1, alpha: 1 });
+}
+
 function noAddpMore() {
   TweenMax.to("#no-addp_more", 0, { top: 0, ease: Quart.easeOut });
   TweenMax.to("#no-addp_more", 0.5, { delay: 0.1, alpha: 1 });
@@ -56,4 +71,26 @@ $(".btn-close, .pop-overlay").on("click", function() {
   });
 });
 
+// fadeIn
+TweenMax.to(".main-title", 0, { alpha: 0, y: 60 });
+TweenMax.to("#main .container", 0, { alpha: 0, y: 80 });
 
+var controller = new ScrollMagic.Controller();
+
+var tween = new TimelineMax().to(".main-title", 1, { alpha: 1, y: 0 });
+var scene = new ScrollMagic.Scene({
+  triggerElement: ".topbar",
+  triggerHook: "onCenter",
+  reverse: false
+})
+  .setTween(tween)
+  .addTo(controller);
+
+var tween1 = new TimelineMax().to("#main .container", 1, { alpha: 1, y: 0, delay: 1});
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#main .container",
+  triggerHook: "onCenter",
+  reverse: false
+})
+  .setTween(tween1)
+  .addTo(controller);
